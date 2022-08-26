0982f1009187a5731213bd6ede5ef4efbd845258
Jacob Bauer <75758754+Jbauer-LRE@users.noreply.github.com>
2022-08-26 17:14:52 -0600

    Add files via upload

---
+++ test_Script.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 26 16:05:49 2022
+
+@author: jbauer
+"""
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+st.write ("hello joel")
