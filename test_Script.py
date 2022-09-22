2d0a48bd19966a9afd8f464bdbc7a71accf788af
Jacob Bauer <75758754+Jbauer-LRE@users.noreply.github.com>
2022-09-22 12:53:10 -0600

    Update test_Script.py

--- test_Script.py
+++ test_Script.py
@@ -6,9 +6,13 @@ Created on Fri Aug 26 16:05:49 2022
 """
 
 import streamlit as st
+import pandas as pd
 #import numpy as np
 #import pandas as pd
 #import matplotlib.pyplot as plt
 
 
 st.write ("hello joel")
+st.write ("hi dave")
+
+data= pd.read_csv('df_sample_data.csv', index_col=0) 
