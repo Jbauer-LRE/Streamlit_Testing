3260116288af4606358e8c8c8d3a6e20fc01be53
Jacob Bauer <75758754+Jbauer-LRE@users.noreply.github.com>
2022-09-22 14:10:59 -0600

    Update test_Script.py

--- test_Script.py
+++ test_Script.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Fri Aug 26 16:05:49 2022
 
@@ -7,6 +6,7 @@ Created on Fri Aug 26 16:05:49 2022
 
 import streamlit as st
 import pandas as pd
+import numpy as np
 #import numpy as np
 #import pandas as pd
 #import matplotlib.pyplot as plt
@@ -18,14 +18,66 @@ st.write ("hi dave")
 
 
 
-st.dataframe(pd.DataFrame({
+
+#####
+
+# bal_df=st.dataframe(pd.DataFrame({
+#     'GMD': ["American Falls- Aberdeen","Bingham","Bonneville-Jefferson","Jefferson-Clark"],
+#     'Balance': [1000, 0, 500, 10]
+# }))
+
+
+# urf_df=st.dataframe(pd.DataFrame({
+#     'GMD': ["American Falls- Aberdeen","Bingham","Bonneville-Jefferson","Jefferson-Clark"],
+#     'URF': [10, 60, 20, 10]
+# }))
+
+
+
+bal_df=(pd.DataFrame({
+     'GMD': ["American Falls- Aberdeen","Bingham","Bonneville-Jefferson","Jefferson-Clark"],
+     'Balance1': [1000, 0, 500, 10],
+     'Balance2': [1000, 0, 500, 10],
+     'Balance3': [0, 100, 500, 10],
+     'Balance4': [0, 0, 500, 10],
+     'Balance5': [1000, 0, 500, 10],
+     'Balance6': [50, 0, 500, 10],
+     'Balance7': [50, 0, 500, 10],
+     'Balance8': [50, 0, 500, 10],
+
+
+ }))
+bal_df=bal_df.T
+bal_arr=np.array(bal_df.iloc[1:9])
+
+
+
+urf_df=(pd.DataFrame({
     'GMD': ["American Falls- Aberdeen","Bingham","Bonneville-Jefferson","Jefferson-Clark"],
-    'Balance': [0, 20, 30, 40]
+    'URF1': [10, 0, 0, 10,],
+    'URF2': [60 ,10, 0, 10],
+    'URF3': [20, 20, 10, 10],
+    'URF4': [10, 60, 50, 10],
+    'URF5': [0,  10, 10, 30,],
+    'URF6': [0,   0, 10, 10],
+    'URF7': [0,  0, 10, 10],
+    'URF8': [0, 0, 10, 10],
+    
+    
 }))
 
-st.dataframe(pd.DataFrame({
-    'GMD': ["American Falls- Aberdeen","Bingham","Bonneville-Jefferson","Jefferson-Clark"],
-    'Balance': [0, 20, 30, 40]
-}))
+urf_arr=urf_df.iloc[0:10,1:9]
+urf_arr=np.array(urf_arr)
 
-[reaches]='Reach':["Milner to King Hill","Neeley-Minidoka","nr Blackfoot-Neeley","shelly-nr Blackfoot",'Heise-Shelly","Ashton-Rexburg"]
+result=bal_arr*urf_arr.T
+result=pd.DataFrame(result)
+st.dataframe(result)
+
+# # reaches=["Milner to King Hill","Neeley-Minidoka","nr Blackfoot-Neeley","shelly-nr Blackfoot","Heise-Shelly","Ashton-Rexburg"]
+
+
+st.markdown("## Party time!")
+st.write("Yay! You're done with this tutorial of Streamlit. Click below to celebrate.")
+btn = st.button("Celebrate!")
+if btn:
+    st.balloons()
