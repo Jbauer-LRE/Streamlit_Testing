0ce0b0fd59550caa29986f62f9ae2e6b4d154524
Jacob Bauer <75758754+Jbauer-LRE@users.noreply.github.com>
2022-10-10 12:26:49 -0600

    Update test_Script.py

--- test_Script.py
+++ test_Script.py
@@ -17,145 +17,145 @@ st.write(parent_path)
 
 
 
-# import matplotlib.pyplot as plt
-# import matplotlib.dates as mdates
-# plt.cla()
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+plt.cla()
 
 
-# st.write("Program active")
+st.write("Program active")
 
 
-# ##################set up program and define inputs
+##################set up program and define inputs
 
-# ## bring in gmd file
-# gmds=pd.read_csv("Model_Grid_RCL_GMDs.csv")
-# ##count by each gmd
-# counts=(gmds['DisName'].value_counts())
+## bring in gmd file
+gmds=pd.read_csv("Model_Grid_RCL_GMDs.csv")
+##count by each gmd
+counts=(gmds['DisName'].value_counts())
 
-# countsx=(gmds.groupby('DisName')['Sort'].nunique())
-# countsx=pd.DataFrame(countsx)
-# countsx=countsx.reset_index()
-# countsx=countsx.rename(columns={"Sort": "number", })
+countsx=(gmds.groupby('DisName')['Sort'].nunique())
+countsx=pd.DataFrame(countsx)
+countsx=countsx.reset_index()
+countsx=countsx.rename(columns={"Sort": "number", })
 
-# #st.write((countsx))
+#st.write((countsx))
 
     
-# ## pumping/change file
+## pumping/change file
 
-# st.write("Reading in blank input sheet")
-# q_df_in=pd.read_csv("InputSheet.csv")
-# q_df_in=q_df_in[0:120]
-# q_df_in.Month=datetime_series = pd.Series( pd.date_range("2013-01-01", periods=120, freq="M"))
+st.write("Reading in blank input sheet")
+q_df_in=pd.read_csv("InputSheet.csv")
+q_df_in=q_df_in[0:120]
+q_df_in.Month=datetime_series = pd.Series( pd.date_range("2013-01-01", periods=120, freq="M"))
 
 
 
-# with st.form('example form') as f:
-#     st.header('INPUT SHEET')
-#     response = AgGrid(q_df_in, editable=True, fit_columns_on_grid_load=True)
-#     st.form_submit_button()
+with st.form('example form') as f:
+    st.header('INPUT SHEET')
+    response = AgGrid(q_df_in, editable=True, fit_columns_on_grid_load=True)
+    st.form_submit_button()
     
-# q_df_in.Month=datetime_series = pd.Series( pd.date_range("2013-01-01", periods=120, freq="M"))
+q_df_in.Month=datetime_series = pd.Series( pd.date_range("2013-01-01", periods=120, freq="M"))
 
 
-# #st.write("hard check on what your inputs were:")
-# #st.write(response['data'])
+#st.write("hard check on what your inputs were:")
+#st.write(response['data'])
 
-# q_df=response['data']
+q_df=response['data']
 
-# st.cache(allow_output_mutation=True)
-# # st.write("simple input")
+st.cache(allow_output_mutation=True)
+# st.write("simple input")
 
-# q_df_x=pd.melt(q_df,id_vars=['Sp',  'Yr', 'Len', 'TS', 'One', 'Tr',   'Month'],value_vars=['Aberdeen-American Falls','Big Lost River','Bingham','Bonneville-Jefferson','Carey Valley', 'Henrys Fork',  'Jefferson', 'Madison', 'Magic Valley',  'North Snake',  'Raft River', ])
-# q_df_x = q_df.rename(columns={'variable': 'DisName'})
+q_df_x=pd.melt(q_df,id_vars=['Sp',  'Yr', 'Len', 'TS', 'One', 'Tr',   'Month'],value_vars=['Aberdeen-American Falls','Big Lost River','Bingham','Bonneville-Jefferson','Carey Valley', 'Henrys Fork',  'Jefferson', 'Madison', 'Magic Valley',  'North Snake',  'Raft River', ])
+q_df_x = q_df.rename(columns={'variable': 'DisName'})
 
-# # #make well file
-# # #format=layer,row,column,q_yr
+# #make well file
+# #format=layer,row,column,q_yr
 
 
 
-# ## write modflow well file
+## write modflow well file
 
-# st.write("Constructing well file, currently configured not to account for any pumping after March 2023")
-# with open('well_slate.txt', 'w') as f:
-#     ## beginning junk
-#     f.writelines(str('      8386        50 '))
-#     f.writelines('\n')
+st.write("Constructing well file, currently configured not to account for any pumping after March 2023")
+with open('well_slate.txt', 'w') as f:
+    ## beginning junk
+    f.writelines(str('      8386        50 '))
+    f.writelines('\n')
 
-#     ## for each stress period
-#     for sp in range(0,120):  ### 6= number of stress periods
-#         print ("sp"+str(sp))
+    ## for each stress period
+    for sp in range(0,120):  ### 6= number of stress periods
+        print ("sp"+str(sp))
         
-#         ###write first line of well file
+        ###write first line of well file
         
-#         #f.write(str('      8386        0')+    ("   #sp"+str(sp)))
-#         #f.write('\n')
+        #f.write(str('      8386        0')+    ("   #sp"+str(sp)))
+        #f.write('\n')
         
         
-#         ##construct well file
+        ##construct well file
         
-#         gmd_q=gmds
-#         gmd_q=gmd_q.merge(countsx)
-#         gmd_q['TS']=sp+1
-#         #q_df1=q_df[::600+sp]
-#         q_df_x=pd.melt(q_df.iloc[[sp]],id_vars=['Sp',  'Yr', 'Len', 'TS', 'One', 'Tr',   'Month'],value_vars=['Aberdeen-American Falls','Big Lost River','Bingham','Bonneville-Jefferson','Carey Valley', 'Henrys Fork',  'Jefferson', 'Madison', 'Magic Valley',  'North Snake',  'Raft River'])
-#         q_df_x = q_df_x.rename(columns={'variable': 'DisName'})
-#         gmd_q1=pd.merge(gmd_q,q_df_x,  on ='DisName',             how ='inner')
-#         gmd_q1['layer']=1
-#         gmd_q1.value=-gmd_q1.value/gmd_q1.number
-#         gmd_q1.value=gmd_q1.value/gmd_q1.Len
+        gmd_q=gmds
+        gmd_q=gmd_q.merge(countsx)
+        gmd_q['TS']=sp+1
+        #q_df1=q_df[::600+sp]
+        q_df_x=pd.melt(q_df.iloc[[sp]],id_vars=['Sp',  'Yr', 'Len', 'TS', 'One', 'Tr',   'Month'],value_vars=['Aberdeen-American Falls','Big Lost River','Bingham','Bonneville-Jefferson','Carey Valley', 'Henrys Fork',  'Jefferson', 'Madison', 'Magic Valley',  'North Snake',  'Raft River'])
+        q_df_x = q_df_x.rename(columns={'variable': 'DisName'})
+        gmd_q1=pd.merge(gmd_q,q_df_x,  on ='DisName',             how ='inner')
+        gmd_q1['layer']=1
+        gmd_q1.value=-gmd_q1.value/gmd_q1.number
+        gmd_q1.value=gmd_q1.value/gmd_q1.Len
         
-#         if gmd_q1.value.sum()==0:
-#             f.writelines(str('      0       0 '))
-#             print ("tesx1")
-#             f.writelines('\n')
+        if gmd_q1.value.sum()==0:
+            f.writelines(str('      0       0 '))
+            print ("tesx1")
+            f.writelines('\n')
             
-#         else:
-#             chunk= gmd_q1[["layer","ROW_ID", "COL_ID","value", "Month"]]
-#             f.writelines(str('      8386        0 '))
-#             f.writelines('\n')
-#             print ("x")
-#             chunk.to_csv(f,index=False,header=False, sep=" ", lineterminator='\n' )
+        else:
+            chunk= gmd_q1[["layer","ROW_ID", "COL_ID","value", "Month"]]
+            f.writelines(str('      8386        0 '))
+            f.writelines('\n')
+            print ("x")
+            chunk.to_csv(f,index=False,header=False, sep=" ", lineterminator='\n' )
 
         
-# st.write("... Modflow Well File Creation Complete")
+st.write("... Modflow Well File Creation Complete")
         
 
         
-# ############ old code for running district-by-district
+############ old code for running district-by-district
         
-# # # #         # for ind in gmds.index[0:8386]:
-# # # #         #     #print(str(1),gmds['ROW_ID'][ind],gmds['COL_ID'][ind],str("#"),gmds['DisName__1'][ind])
-# # # #         #     dist=gmds['DisName__1'][ind]
-# # # #         #     y=q_df[dist].iloc[sp]
-# # # #         #     toprint=(1,gmds['ROW_ID'][ind],gmds['COL_ID'][ind],(y),'#',gmds['DisName__1'][ind])
+# # #         # for ind in gmds.index[0:8386]:
+# # #         #     #print(str(1),gmds['ROW_ID'][ind],gmds['COL_ID'][ind],str("#"),gmds['DisName__1'][ind])
+# # #         #     dist=gmds['DisName__1'][ind]
+# # #         #     y=q_df[dist].iloc[sp]
+# # #         #     toprint=(1,gmds['ROW_ID'][ind],gmds['COL_ID'][ind],(y),'#',gmds['DisName__1'][ind])
         
         
-# # # #         #     line = (' '.join(str(x) for x in toprint))
-# # # #         #     f.writelines(str(line))
-# # # # #         #     f.writelines('\n')
+# # #         #     line = (' '.join(str(x) for x in toprint))
+# # #         #     f.writelines(str(line))
+# # # #         #     f.writelines('\n')
 
-# # # ##  Run Modflow
-# st.write("Running MODFLOW. This may take a few a couple minutes")
+# # ##  Run Modflow
+st.write("Running MODFLOW. This may take a couple minutes")
 
-# exe = "mfusg.exe "
-# mf_nam =  "GMD_eval.nam"
-
-
-# # ###
-# os.system( exe + mf_nam)  #uncomment to make modflow run
+exe = "mfusg.exe "
+mf_nam =  "GMD_eval.nam"
 
 
-# ##important note- this is run with an output control file (OCL) that has been modified to spit out the results for ALL time steps within ALL stress periods
+# ###
+os.system( exe + mf_nam)  #uncomment to make modflow run
 
 
-# st.write("Computing budget for each reach")
-# ###read in the zones file
-# zones=np.loadtxt("zone_array.txt")
-# zones = zones.astype(int)
+##important note- this is run with an output control file (OCL) that has been modified to spit out the results for ALL time steps within ALL stress periods
 
 
-# ##run zonebudget
-# zb = ZoneBudget('Conservation.bud', zones )
+st.write("Computing budget for each reach")
+###read in the zones file
+zones=np.loadtxt("zone_array.txt")
+zones = zones.astype(int)
+
+
+##run zonebudget
+zb = ZoneBudget("./app/Conservation.bud", zones )
 
 # ##record all results
 # zb.to_csv('zonebudtest.csv')
@@ -229,6 +229,4 @@ st.write(parent_path)
 # # mf_list = fp.utils.MfListBudget("NewOUT.lst")
 # # incremental, cumulative = mf_list.get_budget()
 # # df_inc, df_cumulative = mf_list.get_dataframes()
-
-
 
